7148397b0d09bc3d04d122d1b19b7e573c0290b3
donovan-westwater <donovan.westwater@gmail.com>
2023-08-29 21:13:50 -0700

    Metalic and smoothness mask sampling

--- Assets/CustomRenderPipeline/ShaderLibrary/LitInput.hlsl
+++ Assets/CustomRenderPipeline/ShaderLibrary/LitInput.hlsl
@@ -1,9 +1,11 @@
 #ifndef CUSTOM_LIT_INPUT_INCLUDED
 #define CUSTOM_LIT_INPUT_INCLUDED
+#define INPUT_PROP(name) UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, name)
 //This input file is meant to Meta Pass to determine how surfaces should be affected by diffuse reflectiveity
 //Because this is a new meta pass for surfaces, we need a new input file to define the properties
 TEXTURE2D(_BaseMap);
 TEXTURE2D(_EmissionMap); //Texture for emissive materials
+TEXTURE2D(_MaskMap); //Texture for MODS mask map (metallic, occlusion, detail, smoothness)
 SAMPLER(sampler_BaseMap);
 //We want to be able to bundle draw calls to CPU and GPU as batches (if we were using cbuffer)
 //Since cbuffer wont work with per object material properties, then we need to setup GPU instacing instead
@@ -24,26 +26,33 @@ float2 TransformBaseUV(float2 baseUV) {
 
 float4 GetBase(float2 baseUV) {
 	float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
-	float4 color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+	float4 color = INPUT_PROP(_BaseColor);
 	return map * color;
+}
+float4 GetMask(float2 baseUV) {
+	return SAMPLE_TEXTURE2D(_MaskMap, sampler_BaseMap, baseUV);
 }
 float3 GetEmission(float2 baseUV) {
 	float4 map = SAMPLE_TEXTURE2D(_EmissionMap, sampler_BaseMap, baseUV);
-	float4 color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _EmissionColor);
+	float4 color = INPUT_PROP(_EmissionColor);
 	return map.rgb * color.rgb;
 }
 float GetCutoff(float2 baseUV) {
-	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff);
+	return INPUT_PROP(_Cutoff);
 }
 float GetFresnel(float2 baseUV) {
-	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Fresnel);
+	return INPUT_PROP(_Fresnel);
 }
 float GetMetallic(float2 baseUV) {
-	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Metallic);
+	float metallic = INPUT_PROP(_Metallic);
+	metallic *= GetMask(baseUV).r;
+	return metallic;
 }
 
 float GetSmoothness(float2 baseUV) {
-	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Smoothness);
+	float smoothness = INPUT_PROP(_Smoothness);
+	smoothness *= GetMask(baseUV).a;
+	return smoothness;
 }
 
 #endif
--- Assets/CustomRenderPipeline/ShaderLibrary/UnlitInput.hlsl
+++ Assets/CustomRenderPipeline/ShaderLibrary/UnlitInput.hlsl
@@ -1,5 +1,6 @@
 #ifndef CUSTOM_UNLIT_INPUT_INCLUDED
 #define CUSTOM_UNLIT_INPUT_INCLUDED
+#define INPUT_PROP(name) UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, name)
 //This input file is meant to Meta Pass to determine how surfaces should be affected by diffuse reflectiveity
 //Because this is a new meta pass for surfaces, we need a new input file to define the properties
 TEXTURE2D(_BaseMap);
@@ -13,20 +14,20 @@ UNITY_DEFINE_INSTANCED_PROP(float, _Cutoff) //For cutting holes in objects via a
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 float2 TransformBaseUV(float2 baseUV) {
-	float4 baseST = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+	float4 baseST = INPUT_PROP(_BaseMap_ST);
 	return baseUV * baseST.xy + baseST.zw;
 }
 
 float4 GetBase(float2 baseUV) {
 	float4 map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, baseUV);
-	float4 color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+	float4 color = INPUT_PROP(_BaseColor);
 	return map * color;
 }
 float3 GetEmission(float2 baseUV) {
 	return GetBase(baseUV).rgb;
 }
 float GetCutoff(float2 baseUV) {
-	return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _Cutoff);
+	return INPUT_PROP(_Cutoff);
 }
 //DOesn't exist in the unlit pass so return zero 
 float GetMetallic(float2 baseUV) {
