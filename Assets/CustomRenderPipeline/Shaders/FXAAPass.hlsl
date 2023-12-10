b762d8d39fb066397bd529b040d59b3d586f7f11
donovan-westwater <donovan.westwater@gmail.com>
2023-12-10 14:08:37 -0800

    setup basic luma neighborhod

--- Assets/CustomRenderPipeline/Shaders/FXAAPass.hlsl
+++ Assets/CustomRenderPipeline/Shaders/FXAAPass.hlsl
@@ -1,8 +1,15 @@
 #ifndef CUSTOM_FXAA_PASS_INCLUDED
 #define CUSTOM_FXAA_PASS_INCLUDED
+//Manages the convolution process for getting the Luma Neighborhood
+struct LumaNeighborhood {
+	float m, n, e, s, w;
+	float highest, lowest;
+	float range;
+};
 
-float GetLuma(float2 uv) {
-	
+//We want to get the lumanice for a given pixel but also for its neighbors
+float GetLuma(float2 uv, float uOffset = 0.0, float vOffset = 0.0) {
+	uv += float2(uOffset, vOffset)*GetSourceTexelSize().xy;
 #if defined(FXAA_ALPHA_CONTAINS_LUMA)
 	//Use the lumanice calculation we made in the colorgrading step
 	return GetSource(uv).a;
@@ -11,9 +18,24 @@ float GetLuma(float2 uv) {
 	return GetSource(uv).g;
 #endif 
 }
+//We want to get the luminance around the main pixel
+//We use this to get the contrast between the vertical and horizontal directions
+LumaNeighborhood GetLumaNeighborhood(float2 uv) {
+	LumaNeighborhood luma;
+	luma.m = GetLuma(uv);
+	luma.n = GetLuma(uv, 0.0, 1.0);
+	luma.s = GetLuma(uv, 0.0, -1.0);
+	luma.w = GetLuma(uv, -1.0, 0.0);
+	luma.e = GetLuma(uv, 1.0, 0.0);
+	luma.highest = max(max(max(max(luma.m, luma.n), luma.e), luma.s), luma.w);
+	luma.lowest = min(min(min(min(luma.m, luma.n), luma.e), luma.s), luma.w);
+	luma.range = luma.highest - luma.lowest;
+	return luma;
 
+}
 float4 FXAAPassFragment(Varyings input) : SV_TARGET{
-	return GetLuma(input.screenUV);
+	LumaNeighborhood luma = GetLumaNeighborhood(input.screenUV);
+	return luma.range;
 }
 
 #endif
