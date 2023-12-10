ff1e8c39631cb2d82b1e6b2069c6e87dba2dfafb
donovan-westwater <donovan.westwater@gmail.com>
2023-12-09 23:58:26 -0800

    Enabled FXAA and added a lumaninace conversion. We are going to use the greyscale image to find the edges and fix them

--- Assets/CustomRenderPipeline/Shaders/FXAAPass.hlsl
+++ Assets/CustomRenderPipeline/Shaders/FXAAPass.hlsl
@@ -1,8 +1,12 @@
 #ifndef CUSTOM_FXAA_PASS_INCLUDED
 #define CUSTOM_FXAA_PASS_INCLUDED
 
+float GetLuma(float2 uv) {
+	return sqrt(Luminance(GetSource(uv))); //Apply gamma adjustment to get more dark values. Better on eyes
+}
+
 float4 FXAAPassFragment(Varyings input) : SV_TARGET{
-	return GetSource(input.screenUV);
+	return GetLuma(input.screenUV);
 }
 
 #endif
