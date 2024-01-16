138674825dfef1c8e93dbd3688009895e9b775ef
donovan-westwater <donovan.westwater@gmail.com>
2024-01-15 18:50:53 -0800

    I Found a way to get the mip map pyramid to work. I am going to now edit the og setup to use the new setup instead. First I am going to convert this the setup in start into a function. From there I can go back to testing the stroke gen

--- Assets/GenerateMipsTest.cs
+++ Assets/GenerateMipsTest.cs
@@ -9,8 +9,9 @@ using UnityEngine.Rendering;
 public class GenerateMipsTest : MonoBehaviour
 {
     public ComputeShader testMips;
+    Texture2DArray stor;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         RenderTexture test = new RenderTexture(256, 256, 0);
         test.graphicsFormat = GraphicsFormat.R8G8B8A8_SRGB;
@@ -20,15 +21,32 @@ public class GenerateMipsTest : MonoBehaviour
         test.volumeDepth = 8;
         test.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
         test.Create();
-
+        stor = new Texture2DArray(test.width, test.height, test.volumeDepth, test.graphicsFormat, TextureCreationFlags.MipChain);
         testMips.SetTexture(testMips.FindKernel("CSMain"), "Result", test);
         testMips.Dispatch(testMips.FindKernel("CSMain"), 32, 32, 8);
 
         test.GenerateMips();
-
-        SaveRT3DToTexture3DAsset(test,"StrokeGenerationAndRendering/MipGenTest",1);
+        AsyncGPUReadbackRequest[] requests = new AsyncGPUReadbackRequest[test.mipmapCount];
+        for(int m = 0; m < test.mipmapCount; m++)
+        {
+            requests[m] = SaveRT3DToTexture3DAsset(test, "StrokeGenerationAndRendering/MipGenTest", m);
+        }
+        List<int> finshedReqs = new List<int>();
+        int rIndex = 0;
+        while(finshedReqs.Count < test.mipmapCount)
+        {
+            if(requests[rIndex].done && !finshedReqs.Contains(rIndex))
+            {
+                finshedReqs.Add(rIndex);
+            }
+            rIndex++;
+            if (rIndex >= requests.Length) rIndex = 0;
+            yield return new WaitForEndOfFrame();
+        }
+        AssetDatabase.CreateAsset(stor, $"Assets/StrokeGenerationAndRendering/MipGenTest.asset");
+        AssetDatabase.SaveAssetIfDirty(stor);
     }
-    void SaveRT3DToTexture3DAsset(RenderTexture rt3D, string pathWithoutAssetsAndExtension,int mips)
+    AsyncGPUReadbackRequest SaveRT3DToTexture3DAsset(RenderTexture rt3D, string pathWithoutAssetsAndExtension,int mips)
     {
         int width = rt3D.width, height = rt3D.height, depth = rt3D.volumeDepth;
         int mipWidth = width >> mips, mipHeight = height >> mips;
@@ -36,21 +54,23 @@ public class GenerateMipsTest : MonoBehaviour
         NativeArray<int> outputA = new NativeArray<int>(width * height * depth, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
         AsyncGPUReadbackRequest reqStore = AsyncGPUReadback.RequestIntoNativeArray(ref a, rt3D, mips,0,mipWidth,0,mipHeight,0,depth,rt3D.graphicsFormat, (_) =>
         {
-            Texture2DArray output = new Texture2DArray(width, height, depth, rt3D.graphicsFormat, TextureCreationFlags.MipChain);
+            //Texture2DArray output = new Texture2DArray(width, height, depth, rt3D.graphicsFormat, TextureCreationFlags.MipChain);
             //NativeArray<float>.Copy(a, 0, outputA, 0, 1);
             //output.SetPixelData(outputA, 0, 0);
             for (int index = 0; index < depth; index++)
             {
                 var tmpA = a.GetSubArray(index * mipWidth * mipHeight, mipWidth * mipHeight);
-                output.SetPixelData(tmpA, mips, index);
+                stor.SetPixelData(tmpA, mips, index);
             }
-            output.Apply(updateMipmaps: false, makeNoLongerReadable: true);
-            AssetDatabase.CreateAsset(output, $"Assets/{pathWithoutAssetsAndExtension}.asset");
-            AssetDatabase.SaveAssetIfDirty(output);
+            stor.Apply(updateMipmaps: false, makeNoLongerReadable: false);
+            //AssetDatabase.CreateAsset(output, $"Assets/{pathWithoutAssetsAndExtension}.asset");
+            //AssetDatabase.SaveAssetIfDirty(output);
             a.Dispose();
             outputA.Dispose();
-            rt3D.Release();
+            //Graphics.CopyTexture(stor, stor);
+            //stor.Apply();
+            //rt3D.Release();
         });
-        
+        return reqStore;
     }
 }
